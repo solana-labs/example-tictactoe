044cfbd9115b4455d7580ca6f97edad73dad7368
Michael Vines <mvines@gmail.com>
2018-09-27 10:17:29 -0700

    Improve game setup experience: X now shares game key and accepts O

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ async function promptRegEx(rl, promptMessage, allowedResponse) {
 }
 
 function promptPublicKey(rl, promptMessage) {
-  return promptRegEx(rl, promptMessage, /^[1-9A-Za-z]{44}$/);
+  return promptRegEx(rl, promptMessage, /^[1-9A-Za-z]{43,44}$/);
 }
 
 
@@ -57,36 +57,81 @@ export async function main(url) {
     //
     // X initiates the game with the public key from O
     //
-    rl.write('\nYou are X.  Ask O for their identifier.\n\n');
+    //    rl.write('\nYou are X.  Ask O for their identifier.\n\n');
+    //
+    //    const player2Pubkey = await promptPublicKey(rl, 'O identifier: ');
+    //
+    ttt = await TicTacToe.create(connection, myAccount);
 
-    const player2Pubkey = await promptPublicKey(rl, 'O identifier: ');
-
-    ttt = await TicTacToe.Create(connection, myAccount, player2Pubkey);
-
-    rl.write(`Game is ready.  Share the game code with O:\n\n`);
+    rl.write(`\nGame created.  Share the game code with O:\n\n`);
     rl.write(`    ${ttt.gamePublicKey}\n\n`);
+    rl.write(`Waiting for O to join`);
+    try {
+      for (;;) {
+        await ttt.updateGameState();
 
-    await sleep(2000);
+        if (ttt.state.gameState === 'WaitingForO') {
+          rl.write('.');
+        } else if (ttt.state.gameState === 'ORequestPending') {
+          rl.write(`\nAccepting game request from ${ttt.state.playerO}\n`);
+          await ttt.accept();
+        } else {
+          break; // Game accepted by X
+        }
+
+        // For now it's necessary to poll the network for state changes, check
+        // twice a second.
+        await sleep(500);
+      }
+
+    } catch (err) {
+      rl.write(`Unable to locate player O: ${err}\n`);
+      return;
+    }
 
   } else {
-    //
-    // O must share their public key with X, then waits for the public key of the
-    // game from X
-    //
-    rl.write('\nYou are O.  Share your identifier with X:\n\n');
-    rl.write(`    ${myAccount.publicKey}\n\n`);
-    rl.write('and ask them for the game code.\n\n');
-
+    rl.write('\nYou are O.  Ask X for the game code:\n\n');
     const gamePubkey = await promptPublicKey(rl, 'Game code: ');
-    ttt = TicTacToe.Join(connection, myAccount, gamePubkey);
-    rl.write('Joined game\n');
+
+    rl.write(`Requesting to join game`);
+    try {
+      ttt = await TicTacToe.join(connection, myAccount, gamePubkey);
+
+      for (;;) {
+        await ttt.updateGameState();
+        if (ttt.state.inProgress) {
+          break; // Game accepted by X
+        }
+
+        if (ttt.state.gameState !== 'ORequestPending') {
+          throw new Error(`Improper game state: ${ttt.state.gameState}`);
+        }
+
+        rl.write('.');
+        // For now it's necessary to poll the network for state changes, check
+        // twice a second.
+        await sleep(500);
+      }
+
+    } catch (err) {
+      rl.write(`Unable to join game: ${err}\n`);
+      return;
+    }
+    rl.write(`\nGame started with ${ttt.state.playerX}\n`);
   }
 
   //
   // Main game loop
   //
+  rl.write('The game has started.\n');
+  let showBoard = false;
   for (;;) {
     await ttt.updateGameState();
+    if (showBoard) {
+      rl.write(`\n${ttt.state.board}\n`);
+    }
+    showBoard = false;
+
     if (!ttt.state.inProgress) {
       break;
     }
@@ -97,27 +142,26 @@ export async function main(url) {
       await sleep(500);
       continue;
     }
-    rl.write('\n');
-
+    rl.write(`\nYour turn.\n${ttt.state.board}\n`);
     const coords = await promptRegEx(
       rl,
-      `Your turn:\n${ttt.state.board}\nEnter row and column (eg. 1x3): `,
+      'Enter row and column (eg. 1x3): ',
       /^[123]x[123]$/
     );
 
     await ttt.move(Number(coords[0]) - 1, Number(coords[2]) - 1);
-    await ttt.updateGameState();
-    rl.write(`\n${ttt.state.board}\n`);
+    showBoard = true;
   }
 
   //
   // Display result
   //
+  rl.write(`\nGame Over\n=========\n\n${ttt.state.board}\n\n`);
   if (ttt.state.winner) {
-    rl.write('\n\nYou won!\n');
+    rl.write('You won!\n');
   } else if (ttt.state.draw) {
-    rl.write('\n\nDraw.\n');
+    rl.write('Draw.\n');
   } else {
-    rl.write('\n\nYou lost.\n');
+    rl.write('You lost.\n');
   }
 }
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,7 +13,7 @@ import {sleep} from './sleep';
 /**
  * Sign, send and confirm a transaction
  */
-async function sendAndConfirmTransaction(connection, from, transaction) {
+async function sendAndConfirmTransaction(connection, from, transaction, runtimeErrorOk = false) {
   const signature = await connection.sendTransaction(from, transaction);
 
   // Wait up to a couple seconds for a confirmation
@@ -21,6 +21,7 @@ async function sendAndConfirmTransaction(connection, from, transaction) {
   for (;;) {
     const status = await connection.getSignatureStatus(signature);
     if (status == 'Confirmed') return;
+    if (runtimeErrorOk && status == 'ProgramRuntimeError') return;
     await sleep(500);
     if (--i < 0) {
       throw new Error(`Transaction '${signature}' was not confirmed (${status})`);
@@ -33,7 +34,7 @@ export class TicTacToe {
   /**
    * @private
    */
-  constructor(connection, gamePublicKey, playerAccount) {
+  constructor(connection, gamePublicKey, isX, playerAccount) {
     const state = {
       inProgress: false,
       myTurn: false,
@@ -46,6 +47,7 @@ export class TicTacToe {
       this,
       {
         connection,
+        isX,
         playerAccount,
         gamePublicKey,
         state,
@@ -60,37 +62,27 @@ export class TicTacToe {
     return 'CiDwVBFgWV9E5MvXWoLgnEgn2hK7rJikbvfWavzAQz3';
   }
 
-  /**
-   * Create a new game as X
-   */
-  static async Create(connection, player1Account, player2PublicKey) {
+  static async create(connection, playerXAccount) {
     const gameAccount = new Account();
-    const ttt = new TicTacToe(connection, gameAccount.publicKey, player1Account);
+    const ttt = new TicTacToe(connection, gameAccount.publicKey, true, playerXAccount);
 
     // Allocate memory for the game.
     {
       const transaction = SystemProgram.createAccount(
-        player1Account.publicKey,
+        playerXAccount.publicKey,
         gameAccount.publicKey,
         1,
         256, // userdata space
         TicTacToe.programId,
       );
-      await sendAndConfirmTransaction(connection, player1Account, transaction);
+      await sendAndConfirmTransaction(connection, playerXAccount, transaction);
     }
 
-    // Initialize the game
-    const cmd = [
-      'Init',
-      [...Transaction.serializePublicKey(player1Account.publicKey)],
-      [...Transaction.serializePublicKey(player2PublicKey)],
-    ];
-    const userdata = cbor.encode(cmd);
-
+    const userdata = cbor.encode('Init');
     {
       const transaction = new Transaction({
         fee: 0,
-        keys: [gameAccount.publicKey, gameAccount.publicKey],
+        keys: [gameAccount.publicKey, gameAccount.publicKey, playerXAccount.publicKey],
         programId: TicTacToe.programId,
         userdata,
       });
@@ -100,27 +92,29 @@ export class TicTacToe {
   }
 
   /**
-   * Join an existing game as O
+   * Join an existing game as O, X must confirm
    */
-  static Join(connection, player2Account, gamePublicKey) {
-    const ttt = new TicTacToe(connection, gamePublicKey, player2Account);
+  static async join(connection, playerOAccount, gamePublicKey) {
+    const ttt = new TicTacToe(connection, gamePublicKey, false, playerOAccount);
+
+    const userdata = cbor.encode('Join');
+    {
+      const transaction = new Transaction({
+        fee: 0,
+        keys: [playerOAccount.publicKey, gamePublicKey],
+        programId: TicTacToe.programId,
+        userdata,
+      });
+      await sendAndConfirmTransaction(connection, playerOAccount, transaction);
+    }
     return ttt;
   }
 
   /**
-   * Attempt to make a move.
-   * Once this method returns, use `updateGameState()` to fetch the result
+   * Accept a game request from O
    */
-  async move(x, y) {
-    const cmd = [
-      'Move',
-      [...Transaction.serializePublicKey(this.playerAccount.publicKey)],
-      x,
-      y,
-    ];
-
-    const userdata = cbor.encode(cmd);
-
+  async accept() {
+    const userdata = cbor.encode('Accept');
     const transaction = new Transaction({
       fee: 0,
       keys: [this.playerAccount.publicKey, this.gamePublicKey],
@@ -131,16 +125,53 @@ export class TicTacToe {
   }
 
   /**
+   * Reject a game request from O
+   */
+  async reject() {
+    const userdata = cbor.encode('Reject');
+    const transaction = new Transaction({
+      fee: 0,
+      keys: [this.playerAccount.publicKey, this.gamePublicKey],
+      programId: TicTacToe.programId,
+      userdata,
+    });
+    await sendAndConfirmTransaction(this.connection, this.playerAccount, transaction);
+  }
+
+  /**
+   * Attempt to make a move.
+   * Once this method returns, use `updateGameState()` to fetch the result
+   */
+  async move(x, y) {
+    const cmd = [
+      'Move',
+      x,
+      y,
+    ];
+    const userdata = cbor.encode(cmd);
+
+    const transaction = new Transaction({
+      fee: 0,
+      keys: [this.playerAccount.publicKey, this.gamePublicKey],
+      programId: TicTacToe.programId,
+      userdata,
+    });
+    await sendAndConfirmTransaction(this.connection, this.playerAccount, transaction, true);
+  }
+
+  /**
    * Update the `state` field with the latest state
    */
   async updateGameState() {
     const accountInfo = await this.connection.getAccountInfo(this.gamePublicKey);
 
+    //console.log('updateGameState', JSON.stringify(accountInfo));
     const {userdata} = accountInfo;
     const length = userdata.readUInt8(0);
     if (length + 1 >= userdata.length) {
       throw new Error(`Invalid game state`);
     }
+    //console.log('xxxx');
     const rawGameState = cbor.decode(userdata.slice(1));
 
     // TODO: Use joi or superstruct for better input validation
@@ -151,8 +182,6 @@ export class TicTacToe {
 
     // Map rawGameState into `this.state`
     const {game} = rawGameState;
-    const xKey = bs58.encode(game.players[0]);
-    const oKey = bs58.encode(game.players[1]);
 
     // Render the board
     const boardItems = game.grid.map(i => i === 'Free' ? ' ' : i);
@@ -164,34 +193,45 @@ export class TicTacToe {
       boardItems.slice(6,9).join('|'),
     ].join('\n');
 
+    const playerX = bs58.encode(game.player_x);
+    const playerO = game.player_o ? bs58.encode(game.player_o) : null;
+
+    //console.log('yyyxxxx');
     this.state = {
+      gameState: game.state,
       inProgress: false,
       myTurn: false,
       draw: false,
       winner: false,
+      playerX,
+      playerO,
       board,
     };
 
-    if (game.state === 'XMove') {
+    switch (game.state) {
+    case 'WaitingForO':
+      break;
+    case 'ORequestPending':
+      break;
+    case 'XMove':
       this.state.inProgress = true;
-      if (this.playerAccount.publicKey === xKey) {
-        this.state.myTurn = true;
-      }
-    } else if (game.state === 'OMove') {
+      this.state.myTurn = this.isX;
+      break;
+    case 'OMove':
       this.state.inProgress = true;
-      if (this.playerAccount.publicKey === oKey) {
-        this.state.myTurn = true;
-      }
-    } else {
-      if (game.state === 'Draw') {
-        this.state.draw = true;
-      }
-      if (game.state === 'XWon' && this.playerAccount.publicKey === xKey) {
-        this.state.winner = true;
-      }
-      if (game.state === 'OWon' && this.playerAccount.publicKey === oKey) {
-        this.state.winner = true;
-      }
+      this.state.myTurn = !this.isX;
+      break;
+    case 'Draw':
+      this.state.draw = true;
+      break;
+    case 'XWon':
+      this.state.winner = this.isX;
+      break;
+    case 'OWon':
+      this.state.winner = !this.isX;
+      break;
+    default:
+      throw new Error(`Unhandled game state: ${game.state}`);
     }
   }
 }
