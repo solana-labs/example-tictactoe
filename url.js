dd774038322e0c35fb7b1c0451ee3c885a2c012c
Michael Vines <mvines@gmail.com>
2018-11-01 19:53:07 -0700

    Use https endpoint

--- url.js
+++ url.js
@@ -1,5 +1,3 @@
 // @flow
 
-export let url = process.env.LIVE
-  ? 'http://testnet.solana.com:8899'  //TODO: 'https://api.testnet.solana.com';
-  :  'http://localhost:8899';
+export let url = process.env.LIVE ? 'https://api.testnet.solana.com' : 'http://localhost:8899';
