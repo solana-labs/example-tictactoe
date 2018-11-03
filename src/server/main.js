41b285f6c24fd227cecfa03d7f111b56ade56944
Michael Vines <mvines@gmail.com>
2018-11-03 14:29:52 -0700

    Load dashboard immediately on server startup

--- src/server/main.js
+++ src/server/main.js
@@ -3,17 +3,23 @@ import path from 'path';
 import {Connection} from '@solana/web3.js';
 
 import {findDashboard} from './config';
+import {sleep} from '../util/sleep';
 import {url} from '../../url';
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+async function getDashboardPublicKey() {
+  const connection = new Connection(url);
+  const dashboard = await findDashboard(connection);
+  const publicKey = dashboard.publicKey.toBase58();
+  return publicKey;
+}
+
 app.get('/config.json', async (req, res) => {
   console.log('findDashboard request');
   try {
-    const connection = new Connection(url);
-    const dashboard = await findDashboard(connection);
-    const publicKey = dashboard.publicKey.toBase58();
+    const publicKey = await getDashboardPublicKey();
     console.log('findDashboard:', publicKey);
     res.send(JSON.stringify({publicKey})).end();
   } catch (err) {
@@ -22,6 +28,22 @@ app.get('/config.json', async (req, res) => {
   }
 });
 app.use(express.static(path.join(__dirname, '../../dist')));
+
+
+async function loadDashboard() {
+  for (;;) {
+    try {
+      console.log('loading dashboard');
+      const publicKey = await getDashboardPublicKey();
+      console.log('dashboard loaded:', publicKey);
+      return;
+    } catch (err) {
+      console.log('findDashboard at startup failed:', err);
+    }
+    await sleep(500);
+  }
+}
+loadDashboard();
 
 app.listen(port);
 console.log('Listening on port', port);
