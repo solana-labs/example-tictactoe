81e31922a955c9f787990a4cc7ee844f3b90a20b
Michael Vines <mvines@gmail.com>
2018-11-30 08:04:54 -0800

    Prevent LIVE from being used with an incompatible SDK

--- url.js
+++ url.js
@@ -1,5 +1,16 @@
-// @flow
+// @noflow
 
-export let url = process.env.LIVE
+const sdkChannel = require('@solana/web3.js/package.json')['solana-channel'];
+const {LIVE} = process.env;
+
+if (sdkChannel !== 'beta' && LIVE) {
+  // https://api.testnet.solana.com runs the beta channel and we currently have
+  // no equivalent for the edge channel
+  throw new Error(
+    'LIVE not available as SDK channel is currently not set to "beta"',
+  );
+}
+
+export let url = LIVE
   ? 'https://api.testnet.solana.com'
   : 'http://localhost:8899';
