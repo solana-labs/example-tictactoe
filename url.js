c486e4c34309d2e6430bc34de2724c778fa3871a
Michael Vines <mvines@gmail.com>
2018-12-19 19:22:03 -0800

    Use testnetChannelEndpoint to locate RPC endpoint

--- url.js
+++ url.js
@@ -1,16 +1,7 @@
 // @noflow
 
-const sdkChannel = require('@solana/web3.js/package.json')['solana-channel'];
-const {LIVE} = process.env;
+import {testnetChannelEndpoint} from '@solana/web3.js';
 
-if (sdkChannel !== 'beta' && LIVE) {
-  // https://api.testnet.solana.com runs the beta channel and we currently have
-  // no equivalent for the edge channel
-  throw new Error(
-    'LIVE not available as SDK channel is currently not set to "beta"',
-  );
-}
-
-export let url = LIVE
-  ? 'https://api.testnet.solana.com'
+export let url = process.env.LIVE
+  ? testnetChannelEndpoint()
   : 'http://localhost:8899';
