a8c6a8d3df690b8f845541676e1e102f5ffac7f1
Jack May <jack@solana.com>
2020-02-20 09:31:46 -0800

    Point to devnet.solana.com

--- url.js
+++ url.js
@@ -3,12 +3,10 @@
 // To connect to a public testnet, set `export LIVE=1` in your
 // environment. By default, `LIVE=1` will connect to the beta testnet.
 
-import {testnetChannelEndpoint} from '@solana/web3.js';
-
 export let url = process.env.LIVE
-  ? testnetChannelEndpoint(process.env.CHANNEL || 'beta', false)
+  ? 'http://devnet.solana.com:8899'
   : 'http://localhost:8899';
 
 export let urlTls = process.env.LIVE
-  ? testnetChannelEndpoint(process.env.CHANNEL || 'beta', true)
+  ? 'https://devnet.solana.com:8899'
   : 'http://localhost:8899';
