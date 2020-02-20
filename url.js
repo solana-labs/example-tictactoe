d807603aad770f448b867fd87fdf66ba5a66a9c5
Jack May <jack@solana.com>
2020-02-20 12:55:14 -0800

    Bump web3 and use it's testnet URL

--- url.js
+++ url.js
@@ -3,10 +3,12 @@
 // To connect to a public testnet, set `export LIVE=1` in your
 // environment. By default, `LIVE=1` will connect to the beta testnet.
 
+import {testnetChannelEndpoint} from '@solana/web3.js';
+
 export let url = process.env.LIVE
-  ? 'http://devnet.solana.com:8899'
+  ? testnetChannelEndpoint(process.env.CHANNEL || 'stable', false)
   : 'http://localhost:8899';
 
 export let urlTls = process.env.LIVE
-  ? 'https://devnet.solana.com:8899'
+  ? testnetChannelEndpoint(process.env.CHANNEL || 'stable', true)
   : 'http://localhost:8899';
